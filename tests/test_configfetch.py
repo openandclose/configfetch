6d9e055a641f70b7dfab1d37481649f19ea0066f
Open Close <openandclose23@gmail.com>
2020-03-07 17:53:46 +0900

    Refactor more readable config data

--- tests/test_configfetch.py
+++ tests/test_configfetch.py
@@ -1,35 +1,52 @@
 
 import argparse
 import configparser
+import textwrap
 
 import pytest
 
 import configfetch
 
 fetch = configfetch.fetch
-j = '\n'.join
 
+
+def f(string):
+    return textwrap.dedent(string.strip('\n'))
 
 
 class TestInheritance:
 
     def test_iter(self):
-        data = j(['[sec1]', '[sec2]'])
+        data = f("""
+        [sec1]
+        [sec2]
+        """)
         conf = fetch(data)
         assert list(conf.__iter__()) == ['DEFAULT', 'sec1', 'sec2']
 
     def test_iter_option(self):
-        data = j(['[sec1]', 'aa = xxx', 'bb = yyy'])
+        data = f("""
+        [sec1]
+        aa = xxx
+        bb = yyy
+        """)
         conf = fetch(data)
         assert list(conf.sec1.__iter__()) == ['aa', 'bb']
 
     def test_contains(self):
-        data = j(['[sec1]', '[sec2]'])
+        data = f("""
+        [sec1]
+        [sec2]
+        """)
         conf = fetch(data)
         assert 'sec2' in conf
 
     def test_contains_option(self):
-        data = j(['[sec1]', 'aa = xxx', 'bb = yyy'])
+        data = f("""
+        [sec1]
+        aa = xxx
+        bb = yyy
+        """)
         conf = fetch(data)
         assert 'bb' in conf.sec1
 
@@ -37,152 +54,245 @@ class TestInheritance:
 class TestParseConfig:
 
     def test_conf_str(self):
-        data = j(['[sec1]', 'aa = xxx'])
+        data = f("""
+        [sec1]
+        aa = xxx
+        """)
         conf = fetch(data)
         assert conf.sec1.aa == 'xxx'
 
     def test_conf_str_blank(self):
-        data = j(['[sec1]'])
+        data = f("""
+        [sec1]
+        """)
         conf = fetch(data)
         with pytest.raises(configfetch.NoOptionError):
             assert conf.sec1.aa == ''
 
     def test_conf_str_nosection(self):
-        data = j(['[sec1]', 'aa = xxx'])
+        data = f("""
+        [sec1]
+        aa = xxx
+        """)
         conf = fetch(data)
         with pytest.raises(configfetch.NoSectionError):
             assert conf.sec2
 
     def test_conf_str_default(self):
-        data = j(['[DEFAULT]', 'aa = xxx', '[sec1]'])
+        data = f("""
+        [DEFAULT]
+        aa = xxx
+        [sec1]
+        """)
         conf = fetch(data)
         assert conf.sec1.aa == 'xxx'
 
     def test_conf_str_default_nosection(self):
-        data = j(['[DEFAULT]', 'aa = xxx'])
+        data = f("""
+        [DEFAULT]
+        aa = xxx
+        """)
         conf = fetch(data)
         with pytest.raises(configfetch.NoSectionError):
             assert conf.sec1.aa == 'xxx'
 
     def test_conf_str_default_read_section(self):
-        data = j(['[DEFAULT]', 'aa = xxx'])
+        data = f("""
+        [DEFAULT]
+        aa = xxx
+        """)
         conf = fetch(data)
-        data = j(['[sec1]'])
+        data = f("""
+        [sec1]
+        """)
         conf._config.read_string(data)
         assert conf.sec1.aa == 'xxx'
 
     def test_conf_str_default_blank(self):
-        data = j(['[DEFAULT]', '[sec1]'])
+        data = f("""
+        [DEFAULT]
+        [sec1]
+        """)
         conf = fetch(data)
         with pytest.raises(configfetch.NoOptionError):
             assert conf.sec1.aa == ''
 
     def test_conf_str_default_blank_nosection(self):
-        data = j([''])
+        data = ''
         conf = fetch(data)
         with pytest.raises(configfetch.NoSectionError):
             assert conf.sec1.aa == ''
 
     def test_conf_bool(self):
-        data = j(['[sec1]', 'aa = [=BOOL] Yes'])
+        data = f("""
+        [sec1]
+        aa = [=BOOL] Yes
+        """)
         conf = fetch(data)
         assert conf.sec1.aa is True
 
     def test_conf_bool_no(self):
-        data = j(['[sec1]', 'aa = [=BOOL] No'])
+        data = f("""
+        [sec1]
+        aa = [=BOOL] No
+        """)
         conf = fetch(data)
         assert conf.sec1.aa is False
 
     def test_conf_bool_blank(self):
-        data = j(['[sec1]', 'aa = [=BOOL]'])
+        data = f("""
+        [sec1]
+        aa = [=BOOL]
+        """)
         conf = fetch(data)
         with pytest.raises(ValueError):
             assert conf.sec1.aa is False
 
     def test_conf_comma(self):
-        data = j(['[sec1]', 'aa = [=COMMA] xxx1, xxx2, xxx3'])
+        data = f("""
+        [sec1]
+        aa = [=COMMA] xxx1, xxx2, xxx3
+        """)
         conf = fetch(data)
         assert conf.sec1.aa == ['xxx1', 'xxx2', 'xxx3']
 
     def test_conf_comma_indent(self):
-        data = j(['[sec1]', 'aa = [=COMMA] xxx1, xxx2,', '    xxx3'])
+        data = f("""
+        [sec1]
+        aa = [=COMMA] xxx1, xxx2,
+            xxx3
+        """)
         conf = fetch(data)
         assert conf.sec1.aa == ['xxx1', 'xxx2', 'xxx3']
 
     def test_conf_comma_newline(self):
-        data = j(['[sec1]', 'aa = [=COMMA] xxx1, xxx2', '    xxx3'])
+        data = f("""
+        [sec1]
+        aa = [=COMMA] xxx1, xxx2
+            xxx3
+        """)
         conf = fetch(data)
         assert conf.sec1.aa == ['xxx1', 'xxx2\nxxx3']
 
     def test_conf_comma_blank(self):
-        data = j(['[sec1]', 'aa = [=COMMA]'])
+        data = f("""
+        [sec1]
+        aa = [=COMMA]
+        """)
         conf = fetch(data)
         assert conf.sec1.aa == []
 
     def test_conf_line(self):
-        data = j(['[sec1]',
-            'aa = [=LINE]', '    xxx1', '    xxx2', '    xxx3'])
+        data = f("""
+        [sec1]
+        aa = [=LINE]
+            xxx1
+            xxx2
+            xxx3
+        """)
         conf = fetch(data)
         assert conf.sec1.aa == ['xxx1', 'xxx2', 'xxx3']
 
     def test_conf_line_comma(self):
-        data = j(['[sec1]',
-            'aa = [=LINE]', '    xxx1', '    xxx2', '    xxx3, xxx4'])
+        data = f("""
+        [sec1]
+        aa = [=LINE]
+            xxx1
+            xxx2
+            xxx3, xxx4
+        """)
         conf = fetch(data)
         assert conf.sec1.aa == ['xxx1', 'xxx2', 'xxx3, xxx4']
 
     def test_conf_line_blank(self):
-        data = j(['[sec1]', 'aa = [=LINE]'])
+        data = f("""
+        [sec1]
+        aa = [=LINE]
+        """)
         conf = fetch(data)
         assert conf.sec1.aa == []
 
     def test_conf_line_multiblanks(self):
-        data = j(['[sec1]', 'aa = [=LINE]', '    ', '    '])
+        data = f("""
+        [sec1]
+        aa = [=LINE]
+            
+            
+        """)
         conf = fetch(data)
         assert conf.sec1.aa == []
 
     def test_conf_bar_comma(self):
-        data = j(['[sec1]', 'aa = [=COMMA][=BAR] xxx1, xxx2, xxx3'])
+        data = f("""
+        [sec1]
+        aa = [=COMMA][=BAR] xxx1, xxx2, xxx3
+        """)
         conf = fetch(data)
         assert conf.sec1.aa == 'xxx1|xxx2|xxx3'
 
     def test_conf_bar_comma_blank(self):
-        data = j(['[sec1]', 'aa = [=COMMA][=BAR]'])
+        data = f("""
+        [sec1]
+        aa = [=COMMA][=BAR]
+        """)
         conf = fetch(data)
         assert conf.sec1.aa == ''
 
     def test_conf_bar_comma_blank_spaces(self):
-        data = j(['[sec1]', 'aa = [=COMMA] [=BAR]    '])
+        data = f("""
+        [sec1]
+        aa = [=COMMA] [=BAR]    
+        """)
         conf = fetch(data)
         assert conf.sec1.aa == ''
 
     def test_conf_bar_line(self):
-        data = j(['[sec1]', 'aa = [=LINE][=BAR] xxx1', '    xxx2', '    xxx3'])
+        data = f("""
+        [sec1]
+        aa = [=LINE][=BAR] xxx1
+            xxx2
+            xxx3
+        """)
         conf = fetch(data)
         assert conf.sec1.aa == 'xxx1|xxx2|xxx3'
 
     def test_conf_bar_line_blank(self):
-        data = j(['[sec1]', 'aa = [=LINE][=BAR]'])
+        data = f("""
+        [sec1]
+        aa = [=LINE][=BAR]
+        """)
         conf = fetch(data)
         assert conf.sec1.aa == ''
 
     def test_conf_bar_line_blank_spaces(self):
-        data = j(['[sec1]', 'aa = [=LINE] [=BAR]    '])
+        data = f("""
+        [sec1]
+        aa = [=LINE] [=BAR]    
+        """)
         conf = fetch(data)
         assert conf.sec1.aa == ''
 
     def test_conf_cmd(self):
-        data = j(['[sec1]', 'aa = [=CMD] --aaa -b "ccc cc" ddd,dd'])
+        data = f("""
+        [sec1]
+        aa = [=CMD] --aaa -b "ccc cc" ddd,dd
+        """)
         conf = fetch(data)
         assert conf.sec1.aa == ['--aaa', '-b', 'ccc cc', 'ddd,dd']
 
     def test_conf_cmds(self):
-        data = j(['[sec1]', 'aa = [=COMMA][=CMDS] ls *.txt, find . "aaa"'])
+        data = f("""
+        [sec1]
+        aa = [=COMMA][=CMDS] ls *.txt, find . "aaa"
+        """)
         conf = fetch(data)
         assert conf.sec1.aa == [['ls', '*.txt'], ['find', '.', 'aaa']]
 
     def test_conf_fmt(self):
-        data = j(['[sec1]', 'aa = [=FMT] {USER}/data/my.css'])
+        data = f("""
+        [sec1]
+        aa = [=FMT] {USER}/data/my.css
+        """)
         conf = fetch(data, fmts={'USER': '/home/john'})
         assert conf.sec1.aa == '/home/john/data/my.css'
 
@@ -190,17 +300,31 @@ class TestParseConfig:
 class TestParseContexts:
 
     def test_ctx_default_bool(self):
-        data = j(['[DEFAULT]', 'aa = [=BOOL] no', '[sec1]'])
+        data = f("""
+        [DEFAULT]
+        aa = [=BOOL] no
+        [sec1]
+        """)
         conf = fetch(data)
         assert conf.sec1.aa is False
 
     def test_ctx_default_bool_noop(self):
-        data = j(['[DEFAULT]', 'aa = [=BOOL]', '[sec1]', 'aa = no'])
+        data = f("""
+        [DEFAULT]
+        aa = [=BOOL]
+        [sec1]
+        aa = no
+        """)
         conf = fetch(data)
         assert conf.sec1.aa is False
 
     def test_ctx_default_comma(self):
-        data = j(['[DEFAULT]', 'aa = [=COMMA]', '[sec1]', 'aa = xxx1, xxx2, xxx3'])
+        data = f("""
+        [DEFAULT]
+        aa = [=COMMA]
+        [sec1]
+        aa = xxx1, xxx2, xxx3
+        """)
         conf = fetch(data)
         assert conf.sec1.aa == ['xxx1', 'xxx2', 'xxx3']
 
@@ -208,7 +332,11 @@ class TestParseContexts:
 class TestParseFunc:
 
     def test_func_newline(self):
-        data = j(['[sec1]', 'aa = ', '    [=BOOL] no'])
+        data = f("""
+        [sec1]
+        aa =
+            [=BOOL] no
+        """)
         conf = fetch(data)
         assert conf.sec1.aa is False
 
@@ -217,7 +345,11 @@ class TestParseFunc:
 class TestConfigParser:
 
     def test_allow_no_value(self):
-        data = j(['[sec1]', 'aa = ', '    [=BOOL] no'])
+        data = f("""
+        [sec1]
+        aa =
+            [=BOOL] no
+        """)
         config = configparser.ConfigParser(allow_no_value=True)
         config.read_string(data)
         assert config['sec1']['aa'] == '\n[=BOOL] no'
@@ -236,37 +368,55 @@ class TestArgparse:
         return self.parser.parse_args(cmd)
 
     def test_args_and_conf(self):
-        data = j(['[sec1]', 'aa = xxx'])
+        data = f("""
+        [sec1]
+        aa = xxx
+        """)
         args = self.get_args(['--aa', 'axxx'])
         conf = fetch(data, args=args)
         assert conf.sec1.aa == 'axxx'
 
     def test_args_and_conf_short(self):
-        data = j(['[sec1]', 'aa = xxx'])
+        data = f("""
+        [sec1]
+        aa = xxx
+        """)
         args = self.get_args(['-a', 'axxx'])
         conf = fetch(data, args=args)
         assert conf.sec1.aa == 'axxx'
 
     def test_args_and_conf_none(self):
-        data = j(['[sec1]', 'aa = xxx'])
+        data = f("""
+        [sec1]
+        aa = xxx
+        """)
         args = self.get_args([])
         conf = fetch(data, args=args)
         assert conf.sec1.aa == 'xxx'
 
     def test_args_and_conf_const(self):
-        data = j(['[sec1]', 'cc = [=BOOL]'])
+        data = f("""
+        [sec1]
+        cc = [=BOOL]
+        """)
         args = self.get_args(['--cc'])
         conf = fetch(data, args=args)
         assert conf.sec1.cc is True
 
     def test_args_and_conf_const_false(self):
-        data = j(['[sec1]', 'cc = [=BOOL] true'])
+        data = f("""
+        [sec1]
+        cc = [=BOOL] true
+        """)
         args = self.get_args(['--no-cc'])
         conf = fetch(data, args=args)
         assert conf.sec1.cc is False
 
     def test_args_and_conf_dash(self):
-        data = j(['[sec1]', 'ee_eee = xxx'])
+        data = f("""
+        [sec1]
+        ee_eee = xxx
+        """)
         args = self.get_args(['-e', 'axxx'])
         conf = fetch(data, args=args)
         assert conf.sec1.ee_eee == 'axxx'
@@ -282,7 +432,10 @@ class CustomFunc(configfetch.Func):
 class TestCustomize:
 
     def test_customfunc(self):
-        data = j(['[sec1]', 'aa = [=CUSTOM] xxx'])
+        data = f("""
+        [sec1]
+        aa = [=CUSTOM] xxx
+        """)
         conf = fetch(data, Func=CustomFunc)
         assert conf.sec1.aa == 'test'
 
@@ -290,42 +443,72 @@ class TestCustomize:
 class TestDouble:
 
     def test_nooption_nooption(self):
-        data = j(['[sec1]', 'aa = xxx'])
+        data = f("""
+        [sec1]
+        aa = xxx
+        """)
         conf1 = fetch(data)
-        data = j(['[sec1]', 'aa = yyy'])
+        data = f("""
+        [sec1]
+        aa = yyy
+        """)
         conf2 = fetch(data)
         double = configfetch.Double(conf2.sec1, conf1.sec1)
         with pytest.raises(configfetch.NoOptionError):
             assert double.bb == 'zzz'
 
     def test_nooption_blank(self):
-        data = j(['[sec1]', 'aa = xxx'])
+        data = f("""
+        [sec1]
+        aa = xxx
+        """)
         conf1 = fetch(data)
-        data = j(['[sec1]', 'bb ='])
+        data = f("""
+        [sec1]
+        bb =
+        """)
         conf2 = fetch(data)
         double = configfetch.Double(conf2.sec1, conf1.sec1)
         assert double.bb == ''
 
     def test_blank_nooption(self):
-        data = j(['[sec1]', 'bb ='])
+        data = f("""
+        [sec1]
+        bb =
+        """)
         conf1 = fetch(data)
-        data = j(['[sec1]', 'aa = yyy'])
+        data = f("""
+        [sec1]
+        aa = yyy
+        """)
         conf2 = fetch(data)
         double = configfetch.Double(conf2.sec1, conf1.sec1)
         assert double.bb == ''
 
     def test_blank_blank(self):
-        data = j(['[sec1]', 'bb ='])
+        data = f("""
+        [sec1]
+        bb =
+        """)
         conf1 = fetch(data)
-        data = j(['[sec1]', 'bb = [=COMMA]'])
+        data = f("""
+        [sec1]
+        bb = [=COMMA]
+        """)
         conf2 = fetch(data)
         double = configfetch.Double(conf2.sec1, conf1.sec1)
         assert double.bb == ''
 
     def test_plus(self):
-        data = j(['[sec1]', 'aa = [=PLUS] xxx, yyy'])
+        data = f("""
+        [sec1]
+        aa = [=PLUS] xxx, yyy
+        """)
         conf1 = fetch(data)
-        data = j(['[sec1]', 'aa = [=PLUS] -yyy'])
+        data = f("""
+        [sec1]
+        aa = [=PLUS] -yyy
+        """)
         conf2 = fetch(data)
         double = configfetch.Double(conf2.sec1, conf1.sec1)
         assert double.aa == ['xxx']
